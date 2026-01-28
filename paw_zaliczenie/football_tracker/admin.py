6fa6a1b540e13becc70029e3fecd3f3a69f8dd49
MiloszMusialek <milosz.musialek2022@gmail.com>
2026-01-28 22:42:30 +0100

    Zarejestrowano modele w pliku admin

--- paw_zaliczenie/football_tracker/admin.py
+++ paw_zaliczenie/football_tracker/admin.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 from .models import Player, Club, League, Manager, Tournament
 
 # Register your models here.
+admin.site.register(Player)
+admin.site.register(Club)
+admin.site.register(League)
+admin.site.register(Manager)
+admin.site.register(Tournament)
